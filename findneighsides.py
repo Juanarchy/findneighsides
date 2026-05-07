1e72a425896d9dac261e3e3d63da23d0ade25d43
Juan A. Fuenzalida <armagelink67@gmail.com>
2026-05-07 18:20:50 +0200

    Parallel version using python3.14-nogil

--- findneighsides.py
+++ findneighsides.py
@@ -1,4 +1,20 @@
 import numpy as np
+import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
+from timeit import timeit
+
+import itertools
+
+import os
+
+ncpus = os.cpu_count()
+
+# Number of rows to dispatch to each task
+chunk_size = 4
+
+num_workers_domain = [1] + list(range(2, ncpus + 1, 2))
+
+
 
 """
 This script loads a shape (nElems,3) array from a text file whose i-th row contains the index of the (possibly 1-indexed) nodes at each vertex of the i-th triangle 
@@ -21,8 +37,7 @@ of a non-ramified triangulation, and saves 3 text files containing each:
      X-------V-------X            - ElemNeighSides[m,2] == 0
      
 
-Its implemented algorithm iterates over each cell, comparing the three sides of the cell to the three sides of each other cell and positions neighbors and 
-sides accordingly. This "indifferent" approach means that it can efficiently be parallelized through multithread/core/device python APIs. This algorithm can
+This implementation relies on python's multithreading, GIL-free, capabilities, but the idea should be the same for other multiprocessing libraries. This algorithm can
 be easily extended to arbitrary (even higher-dimensional) non-ramified tilings by translating "sides" into "faces" and all their concerning instructions.
 
 It also has the option to load a text file containing the indices of cells to remove from the tiling, but I haven't tested what happens if one removes enough cells
@@ -31,37 +46,58 @@ to leave nodes isolated. I suspect that would be fine, but the resulting answers
 -Juan Andrés Fuenzalida A. Contact: juan[dot]fuenzalidaa[at]sansano[dot]usm[dot]cl
 """
 
-elements=np.loadtxt("ElemNodesMatlab.txt",dtype=int)#load nodes faces array
-elements=elements-1 #turn into 0-index if 1-indexed
-bad_cells=np.loadtxt("celdasmals.txt")
+elements=np.loadtxt("test.txt",dtype=int)#load nodes faces array
+#elements=elements-1 #turn into 0-index if 1-indexed
 
+bad_cells=None
 #bad_cells=np.loadtxt("bad_cells.txt",dtype=int) #Bad cells to further remove
 
 if bad_cells is not None:
     elements=np.delete(elements,bad_cells,0)
 
+ElemNeighs=np.zeros_like(elements)
+ElemNeighSides=np.zeros_like(elements)
 
-ElemNeighs=[]
-ElemNeighSides=[]
-i=0
-for element in elements:
-    neighs=[]
-    neighsides=[]
-    for verts in [(0,1),(1,2),(2,0)]:
-        side=np.take(element,verts)
-        elems_with_nodes=np.vstack(np.where(np.isin(elements,side))).T
-        r,c=np.unique(elems_with_nodes[:,0],return_counts=True)
-        if np.count_nonzero(c-1)>1:
-            neighcell=np.setdiff1d(r[c>1],np.array(i))
-            neighs.append(neighcell[0])
-            x=np.sum(elems_with_nodes[elems_with_nodes[:,0]==neighcell][:,1])
-            neighsides.append(int(np.ceil(-(x-1)*(x-3.5))))
-        else:
-            neighs.append(-1)
-            neighsides.append(-1)
-    ElemNeighs.append(neighs)
-    ElemNeighSides.append(neighsides)
-    i+=1
+def thread_worker(j_verts,lelements):
+    for (j,element) in j_verts:
+        neighs = np.array((0,0,0))
+        neighsides = np.array((0,0,0))
+        for (nside,verts) in enumerate(((0,1),(1,2),(2,0))):
+            side = np.take(element,verts)
+            elems_with_nodes=np.vstack(np.where(np.isin(lelements,side))).T
+            r,c=np.unique(elems_with_nodes[:,0],return_counts=True)
+            if np.count_nonzero(c-1)>1:
+                neighcell=np.setdiff1d(r[c>1],np.array(j))
+                neighs[nside]=neighcell[0]
+                x=np.sum(elems_with_nodes[elems_with_nodes[:,0]==neighcell][:,1])
+                neighsides[nside] = (int(np.ceil(-(x-1)*(x-3.5))))
+            else:
+                neighs[nside]=-1
+                neighsides[nside]=-1
+
+        ElemNeighs[j]=neighs
+        ElemNeighSides[j]=neighsides
+        completed=np.count_nonzero(ElemNeighs)/(ElemNeighs.size)*100
+        print(f'{completed:.2f}% Completed \r')
+
+def run_thread_pool(num_workers):
+    with ThreadPoolExecutor(max_workers=num_workers) as tpe:
+        chunks = itertools.batched(enumerate(elements), chunk_size,strict=False)
+        try:
+            futurez = [tpe.submit(thread_worker,arg,elements.copy()) for arg in chunks]
+            # block until all work finishes
+            concurrent.futures.wait(futurez)
+        finally:
+            # check for exceptions in worker threads
+            [f.result() for f in futurez]
+ts=[]
+for n in (2,3):
+    ElemNeighs=np.zeros_like(elements)
+    ElemNeighSides=np.zeros_like(elements)
+    chunk_size=2**n
+    ts.append((n,timeit('run_thread_pool(ncpus)',number=1, globals=globals())))
+
+print(f'Times: {ts}')
 
 np.savetxt('ElemNeighsFound.txt',ElemNeighs,fmt="%1d")
 np.savetxt('ElemNeighSidesFound.txt',ElemNeighSides,fmt="%1d")
